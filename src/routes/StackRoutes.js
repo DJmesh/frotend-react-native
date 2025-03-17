7ddcfe3c58a165951b7551bac856f24c5b42f5b8
Eduardo Prestes <eduardo.prestes@facens.br>
2025-03-17 14:00:25 -0300

    fix: add new route for register

--- src/routes/StackRoutes.js
+++ src/routes/StackRoutes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// Imports corretos conforme a estrutura:
+// Imports corretos conforme a estrutura
 import HomePage from '../app/(tabs)/home/HomePage';
 import LoginPage from '../app/(auth)/login/LoginPage';
 import DashboardPage from '../app/(tabs)/dashboard/DashboardPage';
+import RegisterPage from '../app/(auth)/register/RegisterPage';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,7 @@ export default function StackRoutes() {
     <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomePage} />
       <Stack.Screen name="Login" component={LoginPage} />
+      <Stack.Screen name="Register" component={RegisterPage} />
       <Stack.Screen name="Dashboard" component={DashboardPage} />
     </Stack.Navigator>
   );
